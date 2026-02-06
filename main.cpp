c0f9c25e95c8f4f48e2383d44fe751d46687dcb8
Gousev Ivan <vanyagousev@gmail.com>
2026-02-06 23:12:52 +0300

    base parsing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
 #include "arguments.h"
 
-int main(int argc, char* argv[]) { return 0; }
+int main(int argc, char* argv[]) {
+	cv::VideoCapture cap;
+	if (handle_arguments(argc, argv, cap)) return 1;
+
+	cv::Mat frame;
+	while (cap.read(frame)) {
+		int keyboard = cv::waitKey(8);
+		cv::imshow("base_frame", frame);
+	}
+
+	return 0;
+}
--- src/arguments.cpp
+++ src/arguments.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+
 #include "arguments.h"
+#include "use.h"
 
 const cv::String params =
 	"{help h usage ? || print this message }"
@@ -8,5 +11,19 @@ const cv::String params =
 	"{file f         || path to video file }";
 
 int handle_arguments(int argc, char* argv[], cv::VideoCapture& cap) {
+	cv::CommandLineParser parser(argc, argv, params);
+	if (!parser.has("file")) {
+		std::cout << "something went wrong on argument parsing. "
+					 "Usage:\n\t"
+				  << argv[0] << " -f=/path/to/file" << std::endl;
+		return 1;
+	}
+	auto path = parser.get<std::string>("file");
+	cap = cv::VideoCapture(path);
+
+	DEBUG_SECTION
+	std::cerr << "Dopening capture on " << path
+			  << "\n\tstatus: " << cap.isOpened() << std::endl;
+
 	return 0;
 }
