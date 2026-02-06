bf7fda5238a3283134798321c8e26c10b392efd8
Gousev Ivan <vanyagousev@gmail.com>
2026-02-06 22:44:45 +0300

    starter cleaning

--- lib/use.h
+++ lib/use.h
@@ -2,6 +2,5 @@
 #define __USE_H__
 
 #define DEBUG_SECTION if constexpr (_DEBUG_ENABLED)
-#define IMSHOW_SECTION if constexpr (!_HEADLESS_ENABLED)
 
-#endif  // !__USE_H__
+#endif	// !__USE_H__
--- src/arguments.cpp
+++ src/arguments.cpp
@@ -1,12 +1,12 @@
 #include "arguments.h"
 
 const cv::String params =
-    "{help h usage ? || print this message }"
-    "{config c       || path to config     }"
-    "{video v        || device number      }"
-    "{port p         || port of http       }"
-    "{file f         || path to video file }";
+	"{help h usage ? || print this message }"
+	// "{config c       || path to config     }"
+	// "{video v        || device number      }"
+	// "{port p         || port of http srv   }"
+	"{file f         || path to video file }";
 
 int handle_arguments(int argc, char* argv[], cv::VideoCapture& cap) {
-  return 0;
+	return 0;
 }
