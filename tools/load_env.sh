8a3d821722bb1b45c3e174f8a3add46cd5bf207b
Gousev Ivan <vanyagousev@gmail.com>
2026-02-07 18:30:52 +0300

    fixes2

--- tools/load_env.sh
+++ tools/load_env.sh
@@ -2,6 +2,7 @@
 
 RED='\033[0;31m'
 NC='\033[0m'
+mkdir -p samples
 cd samples
 curl -O -L -s "https://raw.githubusercontent.com/opencv/opencv/aea90a9e314d220dcaa80a616808afc38e1c78b6/samples/data/vtest.avi" || printf "${RED}VIDEO SAMPLES ARE NOT LOADED${NC}\n"
 cd ..
