a1d9e2d59156c9eb970d32600b63bc58be3f2d45
Gousev Ivan <vanyagousev@gmail.com>
2026-02-09 15:41:30 +0300

    ubuntu 22.04 comp fixes

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ cd .build
 make
 cd ..
 
-python tools/gst-rtsp-srv.py samples/vtest.avi &
+python3 tools/gst-rtsp-srv.py samples/vtest.avi &
 sleep 1 && ./yolohw | python3 ./tools/find_confidence.py
 exit $?
 
--- tools/load_yolo.py
+++ tools/load_yolo.py
@@ -3,16 +3,13 @@ import subprocess
 import sys
 
 def download_and_export_model():
-    """Скачивает модель YOLOv8n и экспортирует её в ONNX."""
     from ultralytics import YOLO
 
-    print("Загрузка модели YOLOv8n.pt...")
+    print("loading YOLOv8n.pt...")
     model = YOLO('yolov8n.pt')
 
-    print("Экспорт модели в формат ONNX...")
     model.export(format='onnx', opset=12, imgsz=640)
-    print("Модель успешно экспортирована в 'yolov8n.onnx'.")
+    print("exported 'yolov8n.onnx'.")
 
 if __name__ == "__main__":
     download_and_export_model()
-    print("\nГотово! Модель 'yolov8n.onnx' готова для использования с OpenCV C++.")
