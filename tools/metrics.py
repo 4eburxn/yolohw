9a2171b71af2fee3c7139be64ce755d1da8fa464
Gousev Ivan <vanyagousev@gmail.com>
2026-02-09 19:25:35 +0300

    ubuntu funny

--- tools/metrics.py
+++ tools/metrics.py
@@ -8,6 +8,6 @@ results = model.val(data='coco8.yaml')
 target_class = 'person'
 class_index = 0
 
-print(f"\nРезультаты для класса {target_class}:")
+print(f"\nFor person:")
 print(f"Precision: {results.box.p[class_index]:.4f}")
 print(f"Recall: {results.box.r[class_index]:.4f}\n")
